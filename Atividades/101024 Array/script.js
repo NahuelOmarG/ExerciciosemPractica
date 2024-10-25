7825afd934a9c9166f21e4276551b3be5e9bc31a
Nahuel Omar Gauna <nahuelgau@gmail.com>
2024-10-24 21:22:40 -0300

    101024 Array 1.1
    Programa atual do array , com as atividades do 8.4 a 8.7

--- Atividades/101024 Array/script.js
+++ Atividades/101024 Array/script.js
@@ -1,14 +1,91 @@
 let numeros = []
 
 function executar83(){
-    //alert (funcionando)
+    // alert("funcionando")
     let numero
-    
-    numero = Math.floor(Math.random()*6)
-     // Math.floor 23.9 >> 23 (arredonda para baixo)
-     // Math.cell 23.9 >> 24  (arredonda para cima)
-     // Math.round 23.9 >> (0.5 para cima 24. ) (0.2 para baixo 23.)
-    numeros.push(numero)
 
+    for(let i=0; i<25; i++){
+        // numero = Math.ceil(Math.random()*6)
+        numero = Math.ceil(Math.random()*1000)
+        numeros.push(numero)
+    }    
     console.log(numeros);
+    document.getElementById('resultado').innerHTML = "Array gerado com  sucesso!"
+}
+
+function executar83b(){
+
+    do{
+        index1 = Number(prompt("Digite um número entre 0 e 24:"))
+    }while(index1 < 0 || index1 > 24)
+
+    do{
+        index2 = Number(prompt("Digite outro número entre 0 e 24:"))
+    }while(index2 < 0 || index2 > 24)
+    // console.log(numeros[index1]);
+    // console.log(numeros[index2]);
+
+    soma = numeros[index1] + numeros[index2]
+    console.log("Soma: ", soma);
+
+    
+
+    // alert(index1)
+
+    // index1 = 0
+    // while(index1 > 0){
+    //     alert(index1)
+    //     index1--
+    // }
+    
+}
+
+function mostrarArray(){
+    // document.getElementById('resultado').innerHTML = "Dado: " + numeros[0] + "<BR>"
+    // document.getElementById('resultado').innerHTML += "Dado: " + numeros[1] + "<BR>"
+    // document.getElementById('resultado').innerHTML += "Dado: " + numeros[2] + "<BR>"
+    
+    document.getElementById('resultado').innerHTML = '<h2>Lançamentos do dado:</h2>'
+    for(let i=0; i<numeros.length; i++){
+        document.getElementById('resultado').innerHTML += "Dado: " + numeros[i] + "<BR>"
+    }
+
+}
+
+function pesquisarComFor(){
+    let valor = Number(prompt("Digite o valor para pesquisar:"))
+    let indice = -1
+    for(let i=0; i<numeros.length; i++){
+        if(numeros[i] == valor){
+            indice = i
+        }
+    }
+
+    if(indice == -1){
+        alert("Não encontrado")
+    }else{
+        alert("Encontrado: " + indice)
+        alert(`Encontrado: ${indice}`)
+    }
+    
+}
+
+function pesquisar(){
+    let valor = Number(prompt("Digite o valor para pesquisar: "))
+    if(numeros.indexOf(valor) == -1){
+        alert("Nana")
+    }else{
+        alert(`Achei: ${numeros.indexOf(valor)}` )
+    }
+}
+function pesquisarFeia1(){ // não faça isso, não seja essa pessoa
+    if(numeros.indexOf(Number(prompt("Digite o valor para pesquisar: "))) == -1){
+        alert("Nana")
+    }else{
+        alert(`Achei: ${numeros.indexOf(valor)}` )
+    }
+}
+
+function executar84(){
+
 }
